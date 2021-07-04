c765cb9cd9b68379fdaa6251a8237bc96b2e4eb0
karthyrio <riokarthy@gmail.com>
2021-07-04 18:10:02 +0530

    2 files modified, 4 added

--- src/Components/Pages/Homepage/HomePage.component.jsx
+++ src/Components/Pages/Homepage/HomePage.component.jsx
@@ -1,39 +1,9 @@
 import React from 'react';
 import './HomePage.styles';
+import Directory from '../../Directory/Directory.component';
 const HomePage = () => (
   <div className="homePage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKET</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 export default HomePage;
