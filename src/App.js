cc1d24e583cdba4344ebc10d4fc077f8669522d6
karthyrio <riokarthy@gmail.com>
2021-07-06 14:48:22 +0530

    router initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './style.css';
 import HomePage from './Components//Pages/Homepage/HomePage.component';
+import { Route } from 'react-router-dom';
 
+const hatsPage = () => {
+  <div>
+    <h1>Hats Page</h1>
+  </div>;
+};
 function App() {
-  return <div><HomePage/></div>;
+  return (
+    <div>
+      <Route exact route="/" component={HomePage} />
+    </div>
+  );
 }
 
 export default App;
