1b6f045ca7cb235d04a7774620802e144192fd38
karthyrio <riokarthy@gmail.com>
2021-07-06 16:12:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './style.css';
 import HomePage from './Components//Pages/Homepage/HomePage.component';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
-const hatsPage = () => {
-  <div>
-    <h1>Hats Page</h1>
-  </div>;
+const hatsPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <Link to="/jacketPage">Jacket Page</Link>
+      <h1>Hats Page</h1>
+    </div>
+  );
 };
+
+const jacketPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Jacket Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <Route exact route="/" component={HomePage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/demo/:hatsId" component={hatsPage} />
+      </Switch>
     </div>
   );
 }
