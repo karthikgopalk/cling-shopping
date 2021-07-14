a838013b4d1c9f061d01113348788340192ac4a3
karthyrio <riokarthy@gmail.com>
2021-07-14 08:33:38 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import Header from './Components/Header/Header.component';
 import HomePage from './Components/Pages/Homepage/HomePage.component';
 import ShopPage from './Components/Pages/Shop/Shop.component';
 import SignInAndSignUp from './Components/Pages/Shop/sign-in-and-sign-up/Sign-In-and-Sign-Up.component';
+import { auth } from './Firebase/Firebase.utils';
 
 import { Route, Switch, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+  unSubscribeFromAuth = null;
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Header/Header.component.jsx
+++ src/Components/Header/Header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './Header.styles.scss';
 import { Link } from 'react-router-dom';
 import { Logo } from '../../Assets/crown.svg';
+import {auth} from '../../Firebase/Firebase.utils'
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className="header">
      <Link className="logo-cantainer" to="/">
      <img className='logo' src="https://www.freepnglogos.com/uploads/crown-png/crown-transparent-png-pictures-icons-and-png-40.png"  alt="header icon" />
@@ -17,6 +18,11 @@ const Header = () => (
       <Link className="option" to="/shop">
         CONTACT
       </Link> 
+      {
+        currentUser?
+        <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div>:
+        <Link className="option" to="/signin">SIGN IN</Link>
+      }
     </div> 
   </div>
 );
